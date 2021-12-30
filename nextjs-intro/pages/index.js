0f3334f6aaa3ab2a8017fa6e2f4a3236a655f708
GEONYEONG SON <thsrjs5@gmail.com>
2021-12-30 22:56:07 +0900

    using getServerSideProps funciton

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,24 +1,53 @@
-import { imageConfigDefault } from 'next/dist/server/image-config';
 import React, { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
 
-const index = () => {
-  const [Movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+const index = ({ results }) => {
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      {!Movies && <h4>Loading...</h4>}
-      {Movies?.map((movie) => {
-        return <div key={movie.id}>{movie.original_title}</div>;
+      {!results && <h4>Loading...</h4>}
+      {results?.map((movie) => {
+        return (
+          <div className="movie" key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        );
       })}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 };
 
 export default index;
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3001/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
