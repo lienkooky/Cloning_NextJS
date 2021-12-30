dcc2a274524e0161b50acb4ec7d7bc38ea6d7c7f
GEONYEONG SON <thsrjs5@gmail.com>
2021-12-30 22:43:28 +0900

    how to set next.config.js

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,17 +1,12 @@
+import { imageConfigDefault } from 'next/dist/server/image-config';
 import React, { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
-
-const API_KEY = '10923b261ba94d897ac6b81148314a3f';
 
 const index = () => {
   const [Movies, setMovies] = useState([]);
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
@@ -19,7 +14,6 @@ const index = () => {
     <div>
       <Seo title="Home" />
       {!Movies && <h4>Loading...</h4>}
-
       {Movies?.map((movie) => {
         return <div key={movie.id}>{movie.original_title}</div>;
       })}
